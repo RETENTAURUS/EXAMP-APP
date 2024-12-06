6588623b0f9f596c131aba628424c970e20d7a86
Gaskinssxxss <bmblx999@gmail.com>
2024-12-06 21:25:26 +0800

    updtx

--- ExampApp/src/models/Pertanyaan.java
+++ ExampApp/src/models/Pertanyaan.java
@@ -29,37 +29,3 @@ public abstract class Pertanyaan {
 
     public abstract void tampilkanPertanyaan();
 }
-
-// package models;
-
-// public abstract class Pertanyaan {
-// private String Question;
-// private double score;
-
-// public Pertanyaan(String Question, double score) {
-// this.Question = Question;
-// this.score = score;
-// }
-
-// public String getQuestion() {
-// return Question;
-// }
-
-// public void setQuestion(String Question) {
-// this.Question = Question;
-// }
-
-// public double getScore() {
-// return score;
-// }
-
-// public void setScore(double score) {
-// this.score = score;
-// }
-
-// public abstract boolean validateAnswer(String userAnswer);
-
-// public void displayQuestion() {
-// System.out.println("Question: " + Question);
-// }
-// }
